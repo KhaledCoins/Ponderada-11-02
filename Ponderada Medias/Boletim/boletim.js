781d068c3041efd3892de6107f09fda8f45ccdf1
Khaledão <eduardo.guardiano@sou.inteli.edu.br>
2025-02-11 12:00:48 -0300

    Add files via upload

---
+++ Ponderada Medias/Boletim/boletim.js
@@ -0,0 +1,26 @@
+// Array de Notas
+const notas = [9.0, 4.5, 7.5]; 
+
+// Soma
+let soma = 0;
+
+// Calcula a soma das notas usando um loop
+for (let i = 0; i < notas.length; i++) {
+    soma += notas[i];
+}
+
+// Calcula a média
+const media = soma / notas.length;
+
+// Mostra as notas
+console.log("Notas:", notas);
+
+// Exibe a média das notas
+console.log("A Média das Notas é:", media.toFixed(2)); // Exibe com duas casas decimais
+
+// Verifica aprovação
+if (media >= 7) {
+    console.log("Status: Aprovado ✅");
+} else {
+    console.log("Status: Reprovado ❌");
+}
